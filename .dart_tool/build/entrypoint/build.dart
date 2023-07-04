94390853c36ca7f0a9500aabe72a9522410af180
Freeman Tang™ <35115094+freemantg@users.noreply.github.com>
2023-07-04 17:25:39 +0100

    feat: Implement SquarePOSHandler for payment processing

---
+++ .dart_tool/build/entrypoint/build.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: directives_ordering
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:build_runner_core/build_runner_core.dart' as _i1;
+import 'package:auto_route_generator/builder.dart' as _i2;
+import 'package:freezed/builder.dart' as _i3;
+import 'package:json_serializable/builder.dart' as _i4;
+import 'package:source_gen/builder.dart' as _i5;
+import 'dart:isolate' as _i6;
+import 'package:build_runner/build_runner.dart' as _i7;
+import 'dart:io' as _i8;
+
+final _builders = <_i1.BuilderApplication>[
+  _i1.apply(
+    r'auto_route_generator:auto_route_generator',
+    [_i2.autoRouteBuilder],
+    _i1.toDependentsOf(r'auto_route_generator'),
+    hideOutput: true,
+  ),
+  _i1.apply(
+    r'auto_route_generator:auto_router_generator',
+    [_i2.autoRouterBuilder],
+    _i1.toDependentsOf(r'auto_route_generator'),
+    hideOutput: false,
+  ),
+  _i1.apply(
+    r'freezed:freezed',
+    [_i3.freezed],
+    _i1.toDependentsOf(r'freezed'),
+    hideOutput: false,
+  ),
+  _i1.apply(
+    r'json_serializable:json_serializable',
+    [_i4.jsonSerializable],
+    _i1.toDependentsOf(r'json_serializable'),
+    hideOutput: true,
+    appliesBuilders: const [r'source_gen:combining_builder'],
+  ),
+  _i1.apply(
+    r'source_gen:combining_builder',
+    [_i5.combiningBuilder],
+    _i1.toNoneByDefault(),
+    hideOutput: false,
+    appliesBuilders: const [r'source_gen:part_cleanup'],
+  ),
+  _i1.applyPostProcess(
+    r'source_gen:part_cleanup',
+    _i5.partCleanup,
+  ),
+];
+void main(
+  List<String> args, [
+  _i6.SendPort? sendPort,
+]) async {
+  var result = await _i7.run(
+    args,
+    _builders,
+  );
+  sendPort?.send(result);
+  _i8.exitCode = result;
+}
